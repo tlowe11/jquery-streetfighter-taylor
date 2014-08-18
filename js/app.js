e0c31f5aa898ef54a42e0d625c986b1e746b16e1
tlowe11 <taylorlowe7@gmail.com>
2014-08-18 17:51:34 -0400

    final commit

---
+++ js/app.js
@@ -0,0 +1,67 @@
+$(document).ready(function (){
+	var isMouseEnter = false;
+	intro();
+
+	$('.ryu').mouseenter(function (){
+		isMouseEnter = true;
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function() {
+		isMouseEnter = false;
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show()
+		.animate(
+			{'left': '300px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '-216px');
+			}
+		);
+	})
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+		//ryu back in ready position
+	});
+	$(document).keydown(function(event) {
+		if (event.keyCode === 88) {
+			console.log('working');
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+		}
+	})
+	.keyup(function(event) {
+		if(event.keyCode === 88){
+			console.log('up');
+			if (isMouseEnter) {
+				$('.ryu-cool').hide();
+				$('.ryu-ready').show();
+			} else {
+				$('.ryu-cool').hide();
+				$('.ryu-still').show();
+			}
+		}
+	});
+});
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
+
+function intro() {
+	$('.ryu, .instructions').hide();
+	$('.logo').fadeIn('slow');
+	$('.logo').fadeOut('slow');
+	$('.ryu, .instructions').delay(1000).fadeIn('slow');
+}
